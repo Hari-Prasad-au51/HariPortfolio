33112ba473e44427e0a5196cbbcc2af599c9bfa2
Harry8096Hari <hari@impactweaver.com>
2025-08-05 12:18:16 +0530

    Switched to HashRouter for GitHub Pages support

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+// import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom' // ✅ Change here
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
+    {/* <BrowserRouter> */}
+    <HashRouter> {/* ✅ Replace BrowserRouter with HashRouter */}
+      <App />
+    </HashRouter>
+    {/* </BrowserRouter> */}
   </React.StrictMode>,
 )
